849c5176284a742105f1d43bc536a98b478cd257
ShuLiang <shuliang@live.cn>
2021-01-21 11:04:24 +0800

    Use sqlx to initialize database

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -2,10 +2,9 @@
 
 # database is just starting up, may not accept connections right away
 sleep 5
-# Run database migration fixes
-movine init
-movine status
-movine fix
+# Run database migration
+sqlx migrate info
+sqlx migrate run
 
 # Run server with cargo watch and systemfd for autoreload
 systemfd --no-pid -s http::0.0.0.0:3000 -- cargo watch -x run
